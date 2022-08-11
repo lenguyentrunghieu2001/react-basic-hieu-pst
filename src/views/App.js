b7bc42946fdf1765768df917906449f421bb0440
Le Nguyen Trung Hieu <lenguyentrunghieu2001@gmail.com>
2022-08-11 12:00:45 +0700

    react

--- src/views/App.js
+++ src/views/App.js
@@ -1,18 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> andsadasdas save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
+          Learn React 1
         </a>
-        <h1>hello world 1</h1>
+        <h1>hello wo1rlsadasd 123</h1>
       </header>
     </div>
   );
